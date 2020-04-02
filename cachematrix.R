2003fa62aa8234ae70ab6c36a8f963b5ee2d172d
Dolmans <Indy.Dolmans@philips.com>
2020-04-02 22:05:57 +0200

    Wrote makeCacheMatrix and cacheSolve Included test to check correctness of implementation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,50 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions in this file are able to cache a matrix and it's inverse
+## and retrieve this cached matrix
 
-## Write a short comment describing this function
-
+## This method takes a matrix and stores this matrix in a matrix object,
+## this method is also able to retrieve the matrix from cache and
+## is able to store and retrieve the inverse of the matrix
 makeCacheMatrix <- function(x = matrix()) {
-
+    invMat <- NULL
+    set <- function(y) {
+        x <<- y
+        invMat <<- NULL
+    }
+    get <- function() x
+    setInverse <- function(inv) invMat <<- inv
+    getInverse <- function() invMat
+    list(set = set, 
+         get = get,
+         setInverse = setInverse,
+         getInverse = getInverse)
 }
 
 
-## Write a short comment describing this function
-
+## This method gets the inverse of a matrix
+## If the inverse of the matrix is stored, the cached matrix will be returned
+## If the inverse is not stored, it will be calculated and stored for later use
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    inverse <- x$getInverse()
+    if(!is.null(inverse)) {
+        message("getting cached matrix")
+        return(inverse)
+    }
+    matrix <- x$get()
+    matrixInv <- solve(matrix)
+    x$setInverse(matrixInv)
+    matrixInv
 }
+
+## Simple test example 
+m1 <- matrix(c(3, 3.2, 3.5, 3.6), 2, 2)
+
+## Make a matrix object out of the matrix
+matrixObject <- makeCacheMatrix(m1)
+
+## Calculating the inverse of the matrix as it is not cached yet
+cacheSolve(matrixObject)
+
+## Retrieving matrix from cache; this should yield the text 'getting cached matrix',
+## as the result should have been stored in the previous function call
+cacheSolve(matrixObject)
+
